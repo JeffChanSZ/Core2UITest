7f3b0576cc1d0fa7369f7cc0ece560ae4f0d0cc7
CHAN SIAW ZHENG <101217869@student.swin.edu.au>
2020-09-24 18:54:48 +1000

    Third Commit (Partially complete)

--- app/src/main/java/au/edu/swin/sdmd/picturethis/Food.kt
+++ app/src/main/java/au/edu/swin/sdmd/picturethis/Food.kt
@@ -1,11 +1,114 @@
 package au.edu.swin.sdmd.picturethis
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RatingBar
+import androidx.appcompat.app.AppCompatActivity
 
 class Food : AppCompatActivity() {
+
+    private var foodName: String? = null
+    private var imageView: ImageView? = null
+    private var imageName: EditText? = null
+    private var cuisine: EditText? = null
+    private var date: EditText? = null
+    private var ratingBar: RatingBar? = null
+    private var updateButton: Button? = null
+    private var food: Image? = null
+
+    private var pname: String? = null
+    private var pcuisine: String? = null
+    private var pdate: String? = null
+
+    private var w: String? = null
+    private var x: String? = null
+    private var z: String? = null
+    private var r: Float? = null
+
+    private var food1: Image? = null
+    private var food2: Image? = null
+    private var food3: Image? = null
+    private var food4: Image? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food)
+
+        val foodImageView = findViewById<ImageView>(R.id.foodImageView)
+        foodName = (findViewById<EditText>(R.id.foodEditText)).toString()
+        cuisine = findViewById<EditText>(R.id.cuisineEditText)
+        date = findViewById<EditText>(R.id.dateEditText)
+        ratingBar = findViewById<RatingBar>(R.id.ratingBar)
+        updateButton = findViewById<Button>(R.id.updateButton)
+
+        val intent = this.intent.also {
+
+            foodName = it.getStringExtra("Food")
+            pname = it.getStringExtra("foodName")
+            pcuisine = it.getStringExtra("cuisine")
+            pdate = it.getStringExtra("date")
+        }
+
+
+
+        when (foodName) {
+            "chickenrice" -> {
+                food1 = intent.getParcelableExtra("food1")
+                imageView?.setImageResource(R.drawable.chickenrice)
+                imageName.setText(food1.getImageName())
+                cuisine.setText(food1.getCuisine())
+                date.setText(food1.getDate())
+            }
+            "nasilemak" -> {
+                food2 = intent.getParcelableExtra("food2")
+                imageView?.setImageResource(R.drawable.nasilemak)
+                imageName.setText(food2.getImageName())
+                cuisine.setText(food2.getCuisine())
+                date.setText(food2.getDate())
+            }
+            "friednoodle" -> {
+                food3 = intent.getParcelableExtra("food3")
+                imageView?.setImageResource(R.drawable.meegoreng)
+                imageName.setText(food3.getImageName())
+                cuisine.setText(food3.getCuisine())
+                date.setText(food3.getDate()))
+            }
+            "satay" -> {
+                food4 = intent.getParcelableExtra("food4")
+                imageView?.setImageResource(R.drawable.satay)
+                imageName.setText(food4.getImageName())
+                cuisine.setText(food4.getCuisine())
+                date.setText(food4.getDate())
+            }
+        }
+
+        updateButton.setOnClickListener(View.OnClickListener {
+            w = imageName.getText().toString()
+            x = cuisine.getText().toString()
+            z = date.getText().toString()
+            r = ratingBar.getRating()
+            food = Image(w, x, z, r)
+
+
+            // Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            val intent = Intent()
+
+            //intent.putExtra("Food", image);
+            intent.putExtra("abc", food)
+            intent.putExtra("food", foodName)
+            // intent.putExtra("name", w);
+            //intent.putExtra("date", z);
+
+            //startActivity(intent);
+            setResult(0, intent)
+            finish()
+        })
+
     }
+
 }
--- app/src/main/java/au/edu/swin/sdmd/picturethis/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/picturethis/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_food.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,11 +14,29 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     //Declare global variable
 
+    private var chickenrice: ImageView? = null
+    private var nasilemak: ImageView? = null
+    private var friednoodle: ImageView? = null
+    private var satay: ImageView? = null
+    
+    private var intent: Intent? = null
+    private var currentFood: String? = null
+    private var foodName: String? = null
+    private var imageName: String? = null
+    private var cuisine: String? = null
+    private var date: String? = null
+    private var rating: Float? = null
+    private var food: Image? = null
 
-    private var foodRating1 = 0
-    private var foodTextView2 = 0
-    private var foodTextView3 = 0
-    private var foodTextView4 = 0
+    private var foodTextView1: TextView? = null
+    private var foodTextView2: TextView? = null
+    private var foodTextView3: TextView? = null
+    private var foodTextView4: TextView? = null
+
+    private var food1: Image? = null
+    private var food2: Image? = null
+    private var food3: Image? = null
+    private var food4: Image? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,97 +48,96 @@ class MainActivity : AppCompatActivity() {
         var nasilemak = findViewById<ImageView>(R.id.nasilemak)
         var friednoodle = findViewById<ImageView>(R.id.friednoodle)
         var satay = findViewById<ImageView>(R.id.satay)
-
+        
         val foodRating1 = findViewById<TextView>(R.id.foodRating1)
         val foodRating2 = findViewById<TextView>(R.id.foodRating2)
         val foodRating3 = findViewById<TextView>(R.id.foodRating3)
         val foodRating4 = findViewById<TextView>(R.id.foodRating4)
 
         intent = Intent(applicationContext, Food::class.java)
-        currentFood = intent.getStringExtra("Food")
+        currentFood = intent.getStringExtra("Current Food")
 
         //collect our intent
         val intent = intent
-        food = intent.getParcelableExtra("Deta")
-        foodName = intent.getStringExtra("food")
+        if (intent != null) {
+            food = intent.getParcelableExtra("Food")
+        }
+        if (intent != null) {
+            foodName = intent.getStringExtra("Food Name")
+        }
 
-        chickenrice = Image("chickenrice", "default", "default", "default", 1.0.toFloat())
-        nasilemak = Image("nasilemak", "default", "default", "default", 1.0.toFloat())
-        friednoodle = Image("friednoodle", "default", "default", "default", 1.0.toFloat())
-        satay = Image("satay", "default", "default", "default", 1.0.toFloat())
+        chickenrice = Image("chickenrice", "default", "default", 1.0.toFloat())
+        nasilemak = Image("nasilemak", "default", "default", 1.0.toFloat())
+        friednoodle = Image("friednoodle", "default", "default", 1.0.toFloat())
+        satay = Image("satay", "default", "default", 1.0.toFloat())
 
     }
 
-    fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        @Nullable data: Intent
-    ) {
+    fun onActivityResult(requestCode: Int, resultCode: Int, @Nullable data: Intent) {
         super.onActivityResult(requestCode, resultCode, data)
         foodName = data.getStringExtra("food")
         if (foodName != null) {
             if (resultCode == 0) {
                 if (requestCode == 1 && foodName == "chickenrice") {
-                    foodTextView1 = data.getParcelableExtra("abc")
-                    imageName = foodTextView1.getImageName()
-                    date = foodTextView1.getDate()
-                    textView7.setText(imageName + "\n" + date)
+                    food1 = data.getParcelableExtra("chickenrice")
+                    imageName = food1.getImageName()
+                    rating = food1.getRating()
+                    foodTextView1?.text = imageName + "\n" + rating
                 }
-                if (requestCode == 2 && foodName == "friedrice") {
-                    img2 = data.getParcelableExtra("abc")
-                    imageName = img2.getImageName()
-                    date = img2.getDate()
-                    textView11.setText(imageName + "\n" + date)
+                if (requestCode == 2 && foodName == "nasilemak") {
+                    food2 = data.getParcelableExtra("friedrice")
+                    imageName = food2.getImageName()
+                    rating = food2.getRating()
+                    foodTextView2?.text = imageName + "\n" + rating
                 }
-                if (requestCode == 3 && foodName == "burger") {
-                    img3 = intent.getParcelableExtra("burger")
-                    imageName = img3.getImageName()
-                    date = img3.getDate()
-                    textView12.setText(imageName + "\n" + date)
+                if (requestCode == 3 && foodName == "friednoodle") {
+                    food3 = intent.getParcelableExtra("friednoodle")
+                    imageName = food3.getImageName()
+                    rating = food3.getRating()
+                    foodTextView3?.text = imageName + "\n" + rating
                 }
-                if (requestCode == 4 && foodName == "pizza") {
-                    img4 = intent.getParcelableExtra("pizza")
-                    imageName = img4.getImageName()
-                    date = img4.getDate()
-                    textView13.setText(imageName + "\n" + date)
+                if (requestCode == 4 && foodName == "satay") {
+                    food4 = intent.getParcelableExtra("satay")
+                    imageName = food4.getImageName()
+                    rating = food4.getDate()
+                    foodTextView4?.text = imageName + "\n" + rating
                 }
             }
         }
         //now collect all property values
-        if (image != null && foodName != null) {
-            imageName = image.getImageName()
-            url = image.getUrl()
-            owner = image.getOwner()
-            date = image.getDate()
-            rating = image.getRating()
+        if (food != null && foodName != null) {
+            imageName = food.getImageName()
+            cuisine = food.getCuisine()
+            date = food.getDate()
+            rating = food.getRating()
             when (foodName) {
-                "pancake" -> {
-                    textView7.setText(imageName + "\n" + date)
-                    img1.setName(image.getImageName())
-                    img1.setUrl(image.getUrl())
-                    img1.setDate(image.getDate())
-                    img1.setOwner(image.getOwner())
+                "chickenrice" -> {
+                    foodTextView1?.text = imageName + "\n" + rating
+                    food1.setName(image.getImageName())
+                    food1.setCuisine(image.getCuisine())
+                    food1.setDate(image.getDate())
+                    food1.setRating(image.getRating())
                 }
-                "friedrice" -> {
-                    textView11.setText(imageName + "\n" + date)
-                    img2.setName(image.getImageName())
-                    img2.setUrl(image.getUrl())
-                    img2.setDate(image.getDate())
-                    img2.setOwner(image.getOwner())
+                "nasilemak" -> {
+                    foodTextView2?.text = imageName + "\n" + rating
+                    food2.setName(image.getImageName())
+                    food2.setCuisine(image.getCuisine())
+                    food2.setDate(image.getDate())
+                    food2.setRating(image.getRating())
                 }
-                "burger" -> {
-                    textView12.setText(imageName + "\n" + date)
-                    img3.setName(image.getImageName())
-                    img3.setUrl(image.getUrl())
-                    img3.setDate(image.getDate())
-                    img3.setOwner(image.getOwner())
+                "friednoodle" -> {
+                    foodTextView3?.text = imageName + "\n" + rating
+                    food3.setName(image.getImageName())
+                    food3.setCuisine(image.getCuisine())
+                    food3.setDate(image.getDate())
+                    food3.setRating(image.getRating())
                 }
-                "pizza" -> {
-                    textView13.setText(imageName + "\n" + date)
-                    img4.setName(image.getImageName())
-                    img4.setUrl(image.getUrl())
-                    img4.setDate(image.getDate())
-                    img4.setOwner(image.getOwner())
+                "satay" -> {
+                    foodTextView4?.text = imageName + "\n" + rating
+                    food4.setName(image.getImageName())
+                    food4.setCuisine(image.getCuisine())
+                    food4.setDate(image.getDate())
+                    food4.setRating(image.getRating())
                 }
             }
         }
@@ -133,27 +151,27 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("Cuisine", cuisineEditText)
         intent.putExtra("Rating", ratingBar)
         when (view.id == R.id.chickenrice) {
-            intent.putExtra("Food", "Chicken Rice")
+            (intent?.putExtra("Food", "Chicken Rice") ?: )
             intent.putExtra("food1", chickenrice) {
                 startActivityForResult(intent, 1)
             }
         }
         when (view.id == R.id.nasilemak) {
-            intent.putExtra("Food", "Nasi Lemak")
+            (intent?.putExtra("Food", "Nasi Lemak") ?: )
             intent.putExtra("food2", nasilemak) {
                 // startActivity(intent);
                 startActivityForResult(intent, 2)
             }
         }
         when (view.id == R.id.friednoodle) {
-            intent.putExtra("Food", "Fried Noodle")
+            (intent?.putExtra("Food", "Fried Noodle") ?: )
             intent.putExtra("food3", friednoodle) {
                 // startActivity(intent);
                 startActivityForResult(intent, 3)
             }
         }
         when (view.id == R.id.satay) {
-            intent.putExtra("Food", "Satay")
+            (intent?.putExtra("Food", "Satay") ?: )
             intent.putExtra("food4", satay) {
                 //startActivity(intent);
                 startActivityForResult(intent, 4)
